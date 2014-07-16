467e8895211588ba27ecbaa3eebc566ee8c61f0b
xLink <xLink@cybershade.org>
2014-07-16 22:27:42 +0100

    commented out the api route

--- routes-api.php
+++ routes-api.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::api(['version' => 'v1', 'prefix' => \Config::get('core::routes.paths.api', 'api')], function () use ($namespace) {
+// Route::api(['version' => 'v1', 'prefix' => \Config::get('core::routes.paths.api', 'api')], function () use ($namespace) {
 
-});
+// });
