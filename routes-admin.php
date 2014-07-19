ddbf9d448fcab7b0eec3bc18314ccfd2b7f5e14b
xLink <xLink@cybershade.org>
2014-07-19 15:21:56 +0100

    added base route for user managing

--- routes-admin.php
+++ routes-admin.php
@@ -1,6 +1,12 @@
 <?php
 
-// if the request matches the admin route group, or we are in console(easier to debug), add the routes
-// if (Request::is(\Config::get('core::routes.paths.admin').'/*') || App::runningInConsole()) {
 
-// }
+Route::group(array('prefix' => 'admin'), function () {
+    $namespace = 'Cysha\Modules\Auth\Controllers\Admin';
+
+    Route::group(array('prefix' => 'users'), function () use ($namespace) {
+
+        Route::get('/', array('as' => 'admin.user.index', 'uses' => $namespace.'\UserController@getDataTableIndex', 'before' => 'permissions'));
+    });
+
+});
