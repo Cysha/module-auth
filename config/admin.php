40137c905c21a3cd6d933070fffde977ee0fcbcb
xLink <xLink@cybershade.org>
2014-07-19 15:23:11 +0100

    added base acp menu routes

---
+++ config/admin.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'acp_menu' => [
+
+        'User Management' => [
+            'admin.user.index'       => '<i class="fa fa-user"></i> Users',
+            //'admin.role.index'       => '<i class="fa fa-users"></i> Roles',
+            //'admin.permission.index' => '<i class="fa fa-users"></i> Permissions',
+        ],
+
+    ],
+];
