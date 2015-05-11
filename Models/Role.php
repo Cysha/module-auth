d2ca25ed021fcf0fc5275f9e4f8c78c01d3977f5
xLink <xLink@cybershade.org>
2015-05-11 12:40:18 +0100

    swapped up the models to grab the authmodel instead

--- Models/Role.php
+++ Models/Role.php
@@ -4,7 +4,7 @@ class Role extends BaseModel
 {
     public function users()
     {
-        return $this->morphedByMany(__NAMESPACE__.'\User', 'caller', 'roleables');
+        return $this->morphedByMany(config('auth.model'), 'caller', 'roleables');
     }
 
     public function permissions()
