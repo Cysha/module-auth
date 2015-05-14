8ff471a6e06c411001e4a3dd733b282100720299
Dan Aldridge <xLink@cybershade.org>
2015-05-14 14:00:35 +0100

    added LockMiddleware

---
+++ Http/Middleware/LockMiddleware.php
@@ -0,0 +1,46 @@
+<?php namespace Cms\Modules\Auth\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+use BeatSwitch\Lock\Callers\CallerLock;
+use BeatSwitch\Lock\Manager;
+
+class LockMiddleware
+{
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard  $auth
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$this->auth->guest()) {
+            $user = $this->auth->user();
+            $lock = app('lock.manager')->caller($user);
+
+            $this->auth->user()->setLock($lock);
+        }
+
+        return $next($request);
+    }
+}
