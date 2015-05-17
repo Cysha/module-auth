2075b6fe8e8cf4c58a80f104cf355bd1a664def3
xLink <xLink@cybershade.org>
2015-05-17 21:08:14 +0100

    added support for all the permissions in the system

--- Database/Seeders/PermissionSeeder.php
+++ Database/Seeders/PermissionSeeder.php
@@ -7,31 +7,32 @@ class PermissionSeeder extends Seeder
 {
     public function run()
     {
-        $permissionGroups = config('cms.auth.permissions');
+        $permissionGroups = array_column(config('cms'), 'permissions');
 
         $seedModel = 'Cms\Modules\Auth\Models\Permission';
-        foreach ($permissionGroups as $type => $permissions) {
+        foreach ($permissionGroups as $group) {
+            foreach ($group as $type => $permissions) {
+                foreach ($permissions as $perm) {
+                    $permission = with(new $seedModel)->fill([
+                        'type'              => 'privilege',
+                        'action'            => $perm,
+                        'resource_type'     => $type,
+                        'resource_id'       => NULL,
+                        'created_at'        => Carbon::now(),
+                        'updated_at'        => Carbon::now(),
+                    ]);
+                    $save = $permission->save();
 
-            foreach($permissions as $perm) {
-                $permission = with(new $seedModel)->fill([
-                    'type'              => 'privilege',
-                    'action'            => $perm,
-                    'resource_type'     => $type,
-                    'resource_id'       => NULL,
-                    'created_at'        => Carbon::now(),
-                    'updated_at'        => Carbon::now(),
-                ]);
-                $save = $permission->save();
+                    if ($save === false) {
+                        print_r($permission->getErrors());
+                        die();
+                    }
 
-                if ($save === false) {
-                    print_r($permission->getErrors());
-                    die();
+                    with(new \Cms\Modules\Auth\Models\Role)->find(1)->permissions()->save($permission);
+                    with(new \Cms\Modules\Auth\Models\Role)->find(2)->permissions()->save($permission);
                 }
 
-                with(new \Cms\Modules\Auth\Models\Role)->find(1)->permissions()->save($permission);
-                with(new \Cms\Modules\Auth\Models\Role)->find(2)->permissions()->save($permission);
             }
-
         }
     }
 }
