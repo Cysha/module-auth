868fe02e38dba85e68cfeba8e0083192b096a506
xLink <xLink@cybershade.org>
2015-05-17 16:10:46 +0100

    added lang files

---
+++ Resources/lang/en/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'permissions.authenticated' => 'You need to be authenticated to get there.',
+    'permissions.unauthorized' => 'You do not have the neccesary permissions for that.',
+
+];
