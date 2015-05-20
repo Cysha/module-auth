d28a397e6f0dee4a94ce0c99e8c8abf923ce0fa7
Dan Aldridge <xLink@cybershade.org>
2015-05-20 14:50:27 +0100

    typo fix

--- Database/Migrations/2015_05_19_205042_auth_create_user_providers_table.php
+++ Database/Migrations/2015_05_19_205042_auth_create_user_providers_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AuthcreateUserProvidersTable extends Migration
+class AuthCreateUserProvidersTable extends Migration
 {
     /**
      * Run the migrations.
