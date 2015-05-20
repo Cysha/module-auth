42b26ccc489fc113b475bc15746770a3a690a727
Dan Aldridge <xLink@cybershade.org>
2015-05-20 12:13:55 +0100

    added an order to the menu items

--- Config/admin.php
+++ Config/admin.php
@@ -8,18 +8,21 @@ return [
                 'route'      => 'admin.user.manager',
                 'text'       => 'Users',
                 'icon'       => 'fa-user',
+                'order'      => 1,
                 'permission' => 'manage@auth_user'
             ],
             [
                 'route'      => 'admin.role.manager',
                 'text'       => 'Roles',
                 'icon'       => 'fa-users',
+                'order'      => 2,
                 'permission' => 'manage@auth_role'
             ],
             [
                 'route'      => 'admin.permission.manager',
                 'text'       => 'Permissions',
                 'icon'       => 'fa-check-square-o',
+                'order'      => 3,
                 'permission' => 'manage@auth_permission'
             ],
         ],
