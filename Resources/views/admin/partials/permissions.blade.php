b3a98b3c0cd7798b0b1005b1c297c25212e75d38
Dan Aldridge <xLink@cybershade.org>
2015-06-09 15:33:18 +0100

    tried to simplify the permission panel some

--- Resources/views/admin/partials/permissions.blade.php
+++ Resources/views/admin/partials/permissions.blade.php
@@ -2,28 +2,29 @@
 @if (count($permissions))
     <div class="col-md-12">
         <div class="row">
-            <div class="col-md-5"><div class="pull-right">
-                <small>Apply to all in group</small>
-            </div></div>
+            <div class="col-md-5"><h5><strong>{{ $title }}</strong></h5></div>
             <div class="col-md-7">
                 <select name="" class="master-select form-control">
-                    <option disabled selected>Please Select</option>
+                    <option disabled selected>Set all the permissions below to:</option>
                     <option class="inherit">Inherit</option>
                     <option class="privilege">Allow</option>
                     <option class="restriction">Deny</option>
                 </select>
             </div>
         </div>
+        <div class="row">
+            <hr>
+        </div>
     @foreach ($permissions as $permission)
         <div class="row permission-row">
-            <div class="col-md-5">
+            <div class="col-md-5"><h5>
                 @if (!empty($permission->readable_name))
                 <span data-toggle="tooltip" data-title="{{ $permission->readable_name }}" data-placement="top" data-trigger="hover focus">
                     <i class="fa fa-question-circle"></i>
                 </span>
                 @endif
                 {{ ucwords(str_replace('.', ' ', $permission->action)) }}
-            </div>
+            </h5></div>
             <div class="col-md-7">
                 @set($type, $role->getPermissionProperty($permission->id, 'type'))
                 <select name="permissions[{{ $permission->action.'@'.$permission->resource_type }}]" class="form-control">
--- Resources/views/admin/role/permissions.blade.php
+++ Resources/views/admin/role/permissions.blade.php
@@ -2,47 +2,99 @@
 
 @section('role-form')
 {!! Former::horizontal_open() !!}
-    <div class="panel panel-default">
+    <div class="panel panel-default panel-permissions">
         <div class="panel-heading">
             <h3 class="panel-title">Permissions</h3>
         </div>
         <div class="panel-body no-padding row">
-            <div class="col-md-3">
+            <?php
+                $tier_one = [];
+                $tier_two = [];
+
+                foreach ($groups as $group) {
+                    list($a, $b) = explode('_', $group);
+                    $tier_one[] = $a;
+                    $tier_two[$a][] = $b;
+                }
+                $tier_one = array_unique($tier_one);
+
+            ?>
+
+            <div class="col-md-2 permissions-nav module">
                 <ul class="nav nav-pills nav-stacked" id="permissions">
-                @set($active, false)
-                @foreach ($groups as $group)
-                    @if($active === false)
-                        @set($active, true)
+                    <li class="disabled">Module</li>
+                @set($active_t1, false)
+                @foreach($tier_one as $module)
+                    @if($active_t1 === false)
+                        @set($active_t1, true)
                         <li class="active">
                     @else
                         <li>
                     @endif
 
-                        <a href="#{{ str_slug($group) }}" data-toggle="pill">{{ ucwords(str_replace('_', ' ', $group)) }}</a>
+                        <a href="#t1_{{ $module }}" data-toggle="pill">{{ ucwords($module) }} <span class="badge pull-right">{{ count($tier_two[$module]) }}</span></a>
                     </li>
                 @endforeach
                 </ul>
             </div>
 
-            <div class="col-md-9">
+            <div class="col-md-10">
                 <div class="tab-content">
-                @set($active, false)
-                @foreach ($groups as $group)
-                    @if($active === false)
-                        @set($active, true)
-                        <div class="tab-pane active" id="{{ str_slug($group) }}">
+                @set($active_t1, false)
+                @foreach($tier_one as $group)
+                    @if($active_t1 === false)
+                        @set($active_t1, true)
+                        <div class="tab-pane active" id="t1_{{ $group }}">
                     @else
-                        <div class="tab-pane" id="{{ str_slug($group) }}">
+                        <div class="tab-pane" id="t1_{{ $group }}">
                     @endif
 
-                        @include(partial('auth::admin.partials.permissions'), [
-                            'permissions' => $permissions->where('resource_type', $group),
-                            'role' => $role
-                        ])
+                    <div class="row">
+                        <div class="col-md-3 permissions-nav model">
+                            <ul class="nav nav-pills nav-stacked" id="permissions">
+                                <li class="disabled">Group</li>
+
+                            @set($active_t2, false)
+                            @foreach($tier_two[$group] as $model)
+                                @if($active_t2 === false)
+                                    @set($active_t2, true)
+                                    <li class="active">
+                                @else
+                                    <li>
+                                @endif
+
+                                    <a href="#t2_{{ $group.'_'.$model }}" data-toggle="pill">{{ ucwords($model) }} <span class="badge pull-right">{{ count($permissions->where('resource_type', $group.'_'.$model)) }}</span></a>
+                                </li>
+                            @endforeach
+                            </ul>
+                        </div>
+
+                        <div class="col-md-9">
+                            <div class="tab-content">
+                            @set($active_t2, false)
+                            @foreach($tier_two[$group] as $model)
+                                @if($active_t2 === false)
+                                    @set($active_t2, true)
+                                    <div class="tab-pane active" id="t2_{{ $group.'_'.$model }}">
+                                @else
+                                    <div class="tab-pane" id="t2_{{ $group.'_'.$model }}">
+                                @endif
+                                    @include(partial('auth::admin.partials.permissions'), [
+                                        'title' => ucwords($group.' > '.$model),
+                                        'permissions' => $permissions->where('resource_type', $group.'_'.$model),
+                                        'role' => $role,
+                                    ])
+                                </div>
+                            @endforeach
+                            </div>
+                        </div>
+                    </div>
+
                     </div>
                 @endforeach
                 </div>
             </div>
+
         </div>
         <div class="panel-footer clearfix">
             <button class="btn-labeled btn btn-success pull-right" type="submit">
