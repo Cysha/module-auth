19c992f3e293f17b271a1e518dae5c86d30a952b
xLink <xLink@cybershade.org>
2015-06-23 15:31:57 +0100

    fixed inline side menus

--- Config/menus.php
+++ Config/menus.php
@@ -32,68 +32,64 @@ return [
     ],
 
     'backend_user_menu' => [
-        'children' => [
-            [
-                'route'      => ['admin.user.edit', ['auth_user_id' => 'segment:3']],
-                'text'       => 'Basic Info',
-                'icon'       => 'fa-file-text-o',
-                'order'      => 1,
-                'permission' => 'manage.update@auth_user'
-            ],
-            [
-                'route'      => ['admin.user.password', ['auth_user_id' => 'segment:3']],
-                'text'       => 'Password',
-                'icon'       => 'fa-key',
-                'order'      => 2,
-                'permission' => 'manage.update@auth_user'
-            ],
-            [
-                'route'      => ['admin.user.avatar', ['auth_user_id' => 'segment:3']],
-                'text'       => 'Avatars',
-                'icon'       => 'fa-picture-o',
-                'order'      => 3,
-                'permission' => 'manage.update@auth_user'
-            ],
-            [
-                'route'      => ['admin.user.role', ['auth_user_id' => 'segment:3']],
-                'text'       => 'Roles',
-                'icon'       => 'fa-users',
-                'order'      => 4,
-                'permission' => 'manage.update@auth_user'
-            ],
-            [
-                'route'      => ['admin.user.permissions', ['auth_user_id' => 'segment:3']],
-                'text'       => 'Permissions',
-                'icon'       => 'fa-check-square-o',
-                'order'      => 5,
-                'permission' => 'manage.update@auth_user'
-            ],
+        [
+            'route'      => ['admin.user.edit', ['auth_user_id' => 'segment:3']],
+            'text'       => 'Basic Info',
+            'icon'       => 'fa-file-text-o',
+            'order'      => 1,
+            'permission' => 'manage.update@auth_user'
+        ],
+        [
+            'route'      => ['admin.user.password', ['auth_user_id' => 'segment:3']],
+            'text'       => 'Password',
+            'icon'       => 'fa-key',
+            'order'      => 2,
+            'permission' => 'manage.update@auth_user'
+        ],
+        [
+            'route'      => ['admin.user.avatar', ['auth_user_id' => 'segment:3']],
+            'text'       => 'Avatars',
+            'icon'       => 'fa-picture-o',
+            'order'      => 3,
+            'permission' => 'manage.update@auth_user'
+        ],
+        [
+            'route'      => ['admin.user.role', ['auth_user_id' => 'segment:3']],
+            'text'       => 'Roles',
+            'icon'       => 'fa-users',
+            'order'      => 4,
+            'permission' => 'manage.update@auth_user'
+        ],
+        [
+            'route'      => ['admin.user.permissions', ['auth_user_id' => 'segment:3']],
+            'text'       => 'Permissions',
+            'icon'       => 'fa-check-square-o',
+            'order'      => 5,
+            'permission' => 'manage.update@auth_user'
         ],
     ],
 
     'backend_role_menu' => [
-        'children' => [
-            [
-                'route'      => ['admin.role.edit', ['auth_role_id' => 'segment:3']],
-                'text'       => 'Basic Info',
-                'icon'       => 'fa-file-text-o',
-                'order'      => 1,
-                'permission' => 'manage.update@auth_role'
-            ],
-            [
-                'route'      => ['admin.role.permissions', ['auth_role_id' => 'segment:3']],
-                'text'       => 'Permissions',
-                'icon'       => 'fa-check-square-o',
-                'order'      => 1,
-                'permission' => 'manage.update@auth_role'
-            ],
-            [
-                'route'      => ['admin.role.permissions', ['auth_role_id' => 'segment:3']],
-                'text'       => 'Manage Users on this Role',
-                'icon'       => 'fa-users',
-                'order'      => 1,
-                'permission' => 'manage.update@auth_role'
-            ],
+        [
+            'route'      => ['admin.role.edit', ['auth_role_id' => 'segment:3']],
+            'text'       => 'Basic Info',
+            'icon'       => 'fa-file-text-o',
+            'order'      => 1,
+            'permission' => 'manage.update@auth_role'
+        ],
+        [
+            'route'      => ['admin.role.permissions', ['auth_role_id' => 'segment:3']],
+            'text'       => 'Permissions',
+            'icon'       => 'fa-check-square-o',
+            'order'      => 1,
+            'permission' => 'manage.update@auth_role'
+        ],
+        [
+            'route'      => ['admin.role.permissions', ['auth_role_id' => 'segment:3']],
+            'text'       => 'Manage Users on this Role',
+            'icon'       => 'fa-users',
+            'order'      => 1,
+            'permission' => 'manage.update@auth_role'
         ],
     ],
 
