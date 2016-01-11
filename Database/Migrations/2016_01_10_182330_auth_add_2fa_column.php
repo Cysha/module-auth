4ede0d2d8c84e6081541ca1ae891ac78ef06bf5a
xLink <xLink@cybershade.org>
2016-01-11 21:40:41 +0000

    fixed case issue in migration

--- Database/Migrations/2016_01_10_182330_auth_add_2fa_column.php
+++ Database/Migrations/2016_01_10_182330_auth_add_2fa_column.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AuthAdd2FaColumn extends Migration
+class AuthAdd2faColumn extends Migration
 {
     /**
      * Run the migrations.
